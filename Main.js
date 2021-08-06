7a0fda4aeea4e34e25145f8efa28609a0ff52a48
ABINAYA2801 <abinaya353@gmail.com>
2021-08-06 09:22:55 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -3,75 +3,103 @@ var finalValue = 0;
 let numArr;
 let OpArr = [];
 
-function displayer(inputStr){
-    this.userStr += inputStr;
-    if(this.userStr[0] != this.userStr.match(/[0-9]/)){
-        displayResult("");
-        this.userStr = this.userStr.substring(1);
+// Dimension newD = new Dimension(480,620);
+// driver.manage().window().setSize(newD);
+
+// getting input from users
+function liveScreen(inputStr){
+    let exp;
+    if(inputStr == '1/x'){
+        exp = this.divide(1,this.userStr);
+        displayResult(exp);
+    }
+    else if(inputStr == '²√'){
+        exp = Math.sqrt(this.userStr);
+        displayResult(exp);
+    }
+    else if(inputStr == 'x²'){
+        exp = Math.pow(this.userStr,2);
+        displayResult(exp);
     }
     else{
-        displayResult(this.userStr);
+        this.userStr += inputStr;
+        if(this.userStr[0] != this.userStr.match(/[0-9]/)){
+            displayResult("");
+            this.userStr = this.userStr.substring(1);
+        }
+        else{
+            displayResult(this.userStr);
+        }
     }
 };
 
+// function to display output as the user enters
 function displayResult(strUser){
     document.getElementById("displayResult").innerHTML = strUser;
 };
 
+// function to perform the arithematic operations
 function calculateUserInput(){
     numArr = this.userStr.split(/[+*%√ \/ -]/);
     for(let i = 0; i < this.userStr.length; i++){
-        if(isNaN(this.userStr[i])){
+        if(this.userStr[i].match(/[+*%√ \/ -]/)){
             OpArr.push(this.userStr[i]);
         }
     }
     for(let i = 0; i < numArr.length; i++){
-        let num1 = parseInt(numArr[i]);
-        let num2 = parseInt(numArr[i+1]);
+        let num1 = parseFloat(numArr[i]);
+        let num2 = parseFloat(numArr[i+1]);
         switch (OpArr[i]) {
             case "+":
-                finalValue = num1 + num2;
+                finalValue = this.add(num1, num2);
                 swapValues(i);
                 break;
             case "-":
-                finalValue = num1 - num2;
+                finalValue = this.sub(num1, num2);
                 swapValues(i);
                 break;
             case "*":
-                finalValue = num1 * num2;
+                finalValue = this.mul(num1, num2);
                 swapValues(i);
                 break;
             case "/":
-                finalValue = num1 / num2;
-                swapValues(i);
-                break;
-            case "p":
-                finalValue = Math.pow(num1 , num2);
+                finalValue = this.divide(num1, num2);
                 swapValues(i);
                 break;
             case "%":
-                finalValue = num1 % num2;
-                swapValues(i);
-                break;
-            case "√":
-                finalValue = Math.sqrt(num1);
-                swapValues(i);
-                break;
-            case "√":
-                finalValue = (num1 * 100) / 10000;
+                finalValue = this.mul(this.divide(num1,100),num2);
                 swapValues(i);
                 break;
         }
     }
-
-    this.userStr = finalValue;
-    displayResult(this.userStr);
+    
+    this.userStr = finalValue.toString();
+    displayResult(finalValue);
+    
+    
     OpArr = [];
-    function swapValues(i){
-        numArr[i] = 0;
-        numArr[i+1] = finalValue;
-        return numArr;
-    }
+}
+
+function add(x,y){
+    z = x + y;
+    return z;
+}
+function sub(x,y){
+    z = x - y;
+    return z;
+}
+function mul(x,y){
+    z = x * y;
+    return z;
+}
+function divide(x,y){
+    z = x / y;
+    return z;
+}
+function swapValues(i){
+    numArr[i] = 0;
+    numArr[i+1] = finalValue;
+    return numArr;
 }
 
 
